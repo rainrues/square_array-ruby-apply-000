954794253fd4ab155ee9013a209e26a8d58704bf
rainrues@gmail.com <rainrues@gmail.com>
2018-08-29 17:39:57 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,9 @@
-# def square_array(array)
-#   squared_array = []
-  
-#   array.each do |num|
-#     squared_array << num ** 2
-#   end
-  
-#   return squared_array
-# end
-
-def square_array_advanced(array)
+def square_array(array)
   squared_array = []
-  squared_array = array.collect { |num| num ** 2 }
+  
+  array.each do |num|
+    squared_array << num ** 2
+  end
+  
+  return squared_array
 end
